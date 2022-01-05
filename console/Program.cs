a2565232ab966f4797cfdd4886ad2fe489252e35
Murat Karaca <murat.github@hotmail.com>
2022-01-06 01:09:45 +0300

    Dotnet Console Programlama

---
+++ console/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace console 
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine("İsminizi girin: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Soyisminizi girin: ");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Hoşgeldin" + " " + name + " " + lastName);
+
+        }
+    }
+}
