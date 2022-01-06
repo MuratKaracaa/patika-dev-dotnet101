cea50bca6ed75db586ac1130a5836698a55ea7cb
Murat Karaca <murat.github@hotmail.com>
2022-01-07 00:23:25 +0300

    exception handling

---
+++ exception-handling/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace exception_handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = Int32.Parse("sdasds");
+            }
+            catch (ArgumentException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Bye..");
+            }
+        }
+    }
+}
