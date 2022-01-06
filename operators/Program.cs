e112ea58ba0b42004918f95c2e51608e14d049f8
Murat Karaca <murat.github@hotmail.com>
2022-01-06 23:33:08 +0300

    operatörler

---
+++ operators/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Atama ve İşlemli Op
+
+            int i = 1;
+            i += 1;
+            i *= 10;
+            i /= 5;
+            i %= 3;
+            i -= 1;
+
+            // Mantıksal Op
+
+            bool b1 = true;
+            bool b2 = false;
+
+            if (b1 && b2)
+                Console.WriteLine("her iki durum");
+
+            if (b1 || b2)
+                Console.WriteLine("bir durum");
+
+            if (!b1)
+                Console.WriteLine("zıttı");
+
+            // İlişkisel
+
+            // Aritmetik
+        }
+    }
+}
