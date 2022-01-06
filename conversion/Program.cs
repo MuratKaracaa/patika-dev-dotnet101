49ebb1e8c0aef4a20d32292e372e6935b3167843
Murat Karaca <murat.github@hotmail.com>
2022-01-06 23:59:31 +0300

    dönüşümler

---
+++ conversion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte i = 5;
+            int j = i;
+
+            string a = "1";
+            byte b = Byte.Parse(a);
+
+            decimal d = 10;
+            float f = (float)d;
+
+            long l = 1000;
+            int x = (int)l;
+
+
+        }
+    }
+}
