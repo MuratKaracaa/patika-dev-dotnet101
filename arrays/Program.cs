a79267281badaebe50c990220891c78b1d3fcf68
Murat Karaca <murat.github@hotmail.com>
2022-01-07 15:08:09 +0300

    arrays

---
+++ arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Bir sayı girin:");
+            int n = int.Parse(Console.ReadLine());
+
+            string[] arr = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = char.ConvertFromUtf32(65 + i);
+            }
+
+            foreach (string c in arr)
+            {
+                Console.Write(c);
+            }
+        }
+    }
+}
