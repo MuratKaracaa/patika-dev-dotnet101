6a2d79121d7819c2b139065881fd348089af35e2
Murat Karaca <murat.github@hotmail.com>
2022-01-07 11:00:12 +0300

    ternary operator

---
+++ ternaryOp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ternaryOp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = DateTime.Now.Hour;
+            string message = hour >= 6 && hour <= 10 ? "Good morning" : hour > 10 && hour <= 14 ? "Good day" : hour <= 18 ? "Good afternoon" : "Good evening";
+            Console.WriteLine(message);
+        }
+    }
+}
