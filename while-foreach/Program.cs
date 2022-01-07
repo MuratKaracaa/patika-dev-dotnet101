e880b2b3745158e50863d58cc668f9b6f11cef4d
Murat Karaca <murat.github@hotmail.com>
2022-01-07 13:39:53 +0300

    while foreach

---
+++ while-foreach/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayı girin: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int i = 0;
+            int sum = 0;
+            while (i <= n)
+            {
+                sum += i;
+                i++;
+            }
+
+            Console.WriteLine(sum);
+
+            char c = 'a';
+            while (c <= 'z')
+            {
+                Console.Write(c);
+                c++;
+            }
+
+            string[] lessons = { "math", "chem", "psy", "phys", "econ" };
+
+            foreach (string el in lessons)
+            {
+                Console.WriteLine(el);
+            }
+
+        }
+    }
+}
