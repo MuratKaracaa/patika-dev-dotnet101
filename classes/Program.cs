98e1a86f7f2e5ef98e7e670bbfe2e6cb9eb2bb5f
Murat Karaca <murat.github@hotmail.com>
2022-01-09 14:58:00 +0300

    classes

---
+++ classes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new()
+            {
+                Name = "Murat",
+                LastName = "Karaca",
+                Num = 23423423,
+                Department = "Sunucu Geliştirme"
+            };
+
+            string e1Info = e1.EmployeeInfo();
+            Console.WriteLine(e1Info);
+        }
+    }
+
+    class Employee
+    {
+        public string Name;
+        public string LastName;
+        public int Num;
+        public string Department;
+
+        public string EmployeeInfo()
+        {
+            return $"Çalışanlarımızdan {this.Name} {this.LastName}'ın numarası {this.Num}'dır ve {this.Department} biriminde çalışmaktadır.";
+        }
+
+    }
+}
